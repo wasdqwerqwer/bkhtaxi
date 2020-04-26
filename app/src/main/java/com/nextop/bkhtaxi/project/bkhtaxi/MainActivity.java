63ef1aa9b87f3f0bbe62ba83dd1ab07293fb86f8
jybill01 <49678607+jybill01@users.noreply.github.com>
2020-04-26 18:02:38 +0900

    map setting

---
+++ app/src/main/java/com/nextop/bkhtaxi/project/bkhtaxi/MainActivity.java
@@ -0,0 +1,39 @@
+package com.nextop.bkhtaxi.project.bkhtaxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.ViewGroup;
+import net.daum.mf.map.api.MapView;
+
+import java.security.MessageDigest;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.nextop.project.nextoptaxi", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.i("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (Exception e) {
+            Log.e("MainActivity", e.getStackTrace().toString());
+        }
+    }
+}
